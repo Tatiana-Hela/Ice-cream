98b287ba3d295e396ed8cbcf16129968dce0dcdb
Tatiana-Hela <108082909+Tatiana-Hela@users.noreply.github.com>
2023-01-15 00:05:56 +0000

    Deploying to gh-pages from @ Tatiana-Hela/Ice-cream@3c84916499d6d1a0048dc8fd389d1a65e6176a27 🚀

--- index.95c43c42.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelectorAll("[data-modal-about-open]"),closeModalBtn:document.querySelector("[data-modal-about-close]"),modal:document.querySelector("[data-modal-about]")};function e(){document.body.classList.toggle("no-scroll"),o.modal.classList.toggle("is-hidden"),document.body.classList.toggle("modal-open")}console.log(o.openModalBtn),o.openModalBtn.forEach((function(o){o.addEventListener("click",e)})),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.95c43c42.js.map
---
+++ index.b1d3c28e.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalBtn:document.querySelectorAll("[data-modal-about-open]"),closeModalBtn:document.querySelector("[data-modal-about-close]"),modal:document.querySelector("[data-modal-about]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
+//# sourceMappingURL=index.b1d3c28e.js.map
