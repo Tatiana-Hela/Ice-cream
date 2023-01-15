99ed9e2ed7b985bd486979f439c56b3fdbc87f93
Tatiana-Hela <108082909+Tatiana-Hela@users.noreply.github.com>
2023-01-15 00:00:16 +0000

    Deploying to gh-pages from @ Tatiana-Hela/Ice-cream@9f2c1154525a76ba86277a21d367ab16a93d460e 🚀

--- index.b1d3c28e.js
+++
@@ -1,2 +0,0 @@
-(()=>{const o={openModalBtn:document.querySelectorAll("[data-modal-about-open]"),closeModalBtn:document.querySelector("[data-modal-about-close]"),modal:document.querySelector("[data-modal-about]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
-//# sourceMappingURL=index.b1d3c28e.js.map
--- index.bb0d5287.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[data-modal-franchise-open]"),closeModalBtn:document.querySelector("[data-modal-franchise-close]"),modal:document.querySelector("[data-modal-franchise]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
-//# sourceMappingURL=index.bb0d5287.js.map
